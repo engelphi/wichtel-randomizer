5289bc80e6871796ee5d0d6718b86ab8651cb354
engelphi <philipp.engel.1990@googlemail.com>
2022-04-27 20:52:32 +0200

    move logic into library file

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,7 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-use serde::Deserialize;
-use std::collections::HashMap;
 use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
 use std::result::Result;
-
-#[derive(Debug, Deserialize)]
-struct Persons {
-    persons: Vec<String>,
-}
-
-fn read_users_from_file<P: AsRef<Path>>(path: P) -> Result<Persons, Box<dyn Error>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    let persons = serde_json::from_reader(reader)?;
-
-    return Ok(persons);
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let json_file = std::env::args().nth(1).expect("no input file given");
-    let path = Path::new(&json_file);
-    let persons = read_users_from_file(path).expect("no persons in file");
-
-    let mut choices = persons.persons.clone();
-    let mut rng = thread_rng();
-    let mut map: HashMap<String, String> = HashMap::new();
-    for person in persons.persons.iter() {
-        let available_choices = choices
-            .iter()
-            .filter(|name| **name != *person)
-            .collect::<Vec<&String>>();
-        let choice = available_choices.choose(&mut rng).expect("No choices left");
-        map.insert(
-            person.clone().as_str().to_string(),
-            choice.clone().as_str().to_string(),
-        );
-        let index = choices
-            .iter()
-            .position(|name| *name == **choice)
-            .expect("choice not found");
-        choices.remove(index);
-    }
-    println!(
-        "Result: {}",
-        serde_json::to_string(&map).expect("Failed to serialize response")
-    );
-    return Ok(());
+    return wichtel_randomizer::randomize(json_file);
 }
