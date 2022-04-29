025bafb03e9330625807c05369ecdf654df0be03
engelphi <philipp.engel.1990@googlemail.com>
2022-04-29 15:47:59 +0200

    add commandline interface and option to write results to file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use clap::Parser;
 use std::error::Error;
 use std::result::Result;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let json_file = std::env::args().nth(1).expect("no input file given");
-    return wichtel_randomizer::randomize(json_file);
+    let app = wichtel_randomizer::App::new(wichtel_randomizer::Args::parse());
+    return app.randomize();
 }
